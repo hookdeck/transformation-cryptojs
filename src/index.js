4793b833d6fd10d0bb5b386227f7451eb6c39596
Maurice Kherlakian <mkherlakian@gmail.com>
2022-08-17 16:12:35 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var crypto = require("crypto-js")
+
+function getSignatureKey(key, dateStamp, regionName, serviceName) {
+  var kDate = crypto.HmacSHA256(dateStamp, "AWS4" + key)
+  var kRegion = crypto.HmacSHA256(regionName, kDate)
+  var kService = crypto.HmacSHA256(serviceName, kRegion)
+  var kSigning = crypto.HmacSHA256("aws4_request", kService)
+  return kSigning
+}
+
+addHandler("transform", (request, context) => {
+  const sig = getSignatureKey("KEYEXAMPLE", "20150830", "us-east-1", "iam")
+
+  console.log(sig)
+  // Transform the request object then return it.
+  return request
+})
